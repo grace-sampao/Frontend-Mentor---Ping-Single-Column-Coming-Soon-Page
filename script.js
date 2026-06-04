a9aa078e00421143a780e6fd26a7eaa7e0f13e5f
grace-sampao <gnsampao@gmail.com>
2026-06-04 13:34:51 +0300

    Updated files.

--- script.js
+++ script.js
@@ -1,34 +1,82 @@
-const subscriptionForm = document.getElementById('subscription-form');
-const emailInput = document.getElementById('email-input');
-const mobileErrorMessage = document.getElementById('mobile-error-message');
-const desktopErrorMessage = document.getElementById('desktop-error-message');
+const subscriptionForm = document.getElementById("subscription-form");
+const emailInput = document.getElementById("email-input");
+const mobileErrorMessage = document.getElementById("mobile-error-message");
+const desktopErrorMessage = document.getElementById("desktop-error-message");
 
 const pattern = /^[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
 
-subscriptionForm.addEventListener('submit', submitForm);
+subscriptionForm.addEventListener("submit", submitForm);
 
 function submitForm(event) {
   if (emailInput.value == false && screen.width < 1024) {
-    mobileErrorMessage.innerHTML = "Whoops! It looks like you forgot to add your email.";
-    mobileErrorMessage.style.display = 'block';
-    emailInput.style.borderColor = 'hsl(354, 100%, 66%)';
+    mobileErrorMessage.innerHTML =
+      "Whoops! It looks like you forgot to add your email.";
+    mobileErrorMessage.style.display = "block";
+    emailInput.style.borderColor = "hsl(354, 100%, 66%)";
 
     event.preventDefault();
   } else if (emailInput.value == false && screen.width >= 1024) {
-    desktopErrorMessage.innerHTML = "Whoops! It looks like you forgot to add your email.";
-    desktopErrorMessage.style.display = 'block';
-    emailInput.style.borderColor = 'hsl(354, 100%, 66%)';
+    desktopErrorMessage.innerHTML =
+      "Whoops! It looks like you forgot to add your email.";
+    desktopErrorMessage.style.display = "block";
+    emailInput.style.borderColor = "hsl(354, 100%, 66%)";
 
     event.preventDefault();
   } else if (pattern.test(emailInput.value) === false && screen.width < 1024) {
-    mobileErrorMessage.style.display = 'block';
-    emailInput.style.borderColor = 'hsl(354, 100%, 66%)';
+    mobileErrorMessage.style.display = "block";
+    emailInput.style.borderColor = "hsl(354, 100%, 66%)";
 
     event.preventDefault();
   } else if (pattern.test(emailInput.value) === false && screen.width >= 1024) {
-    desktopErrorMessage.style.display = 'block';
-    emailInput.style.borderColor = 'hsl(354, 100%, 66%)';
+    desktopErrorMessage.style.display = "block";
+    emailInput.style.borderColor = "hsl(354, 100%, 66%)";
 
     event.preventDefault();
   }
 }
+
+const facebookIcon = document.getElementById("icon-facebook");
+const twitterIcon = document.getElementById("icon-twitter");
+const instagramIcon = document.getElementById("icon-instagram");
+
+facebookIcon.onmouseenter = function () {
+  facebookIcon.innerHTML = `<path
+    d="M80 299.3l0 212.7 116 0 0-212.7 86.5 0 18-97.8-104.5 0 0-34.6c0-51.7 20.3-71.5 72.7-71.5 16.3 0 29.4 .4 37 1.2l0-88.7C291.4 4 256.4 0 236.2 0 129.3 0 80 50.5 80 159.4l0 42.1-66 0 0 97.8 66 0z"
+    fill="white"
+  />`;
+};
+
+facebookIcon.onmouseleave = function () {
+  facebookIcon.innerHTML = `<path
+    d="M80 299.3l0 212.7 116 0 0-212.7 86.5 0 18-97.8-104.5 0 0-34.6c0-51.7 20.3-71.5 72.7-71.5 16.3 0 29.4 .4 37 1.2l0-88.7C291.4 4 256.4 0 236.2 0 129.3 0 80 50.5 80 159.4l0 42.1-66 0 0 97.8 66 0z"
+    fill="hsl(223, 87%, 63%)"
+  />`;
+};
+
+twitterIcon.onmouseenter = function () {
+  twitterIcon.innerHTML = `<path
+    d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103l0-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"
+    fill="white"
+  />`;
+};
+
+twitterIcon.onmouseleave = function () {
+  twitterIcon.innerHTML = `<path
+    d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103l0-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"
+    fill="hsl(223, 87%, 63%)"
+  />`;
+};
+
+instagramIcon.onmouseenter = function () {
+  instagramIcon.innerHTML = `<path
+    d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+    fill="white"
+  />`;
+};
+
+instagramIcon.onmouseleave = function () {
+  instagramIcon.innerHTML = `<path
+    d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+    fill="hsl(223, 87%, 63%)"
+  />`;
+};
